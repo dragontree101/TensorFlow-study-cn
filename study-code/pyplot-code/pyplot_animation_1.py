beee77dfc9d368358311380671ee908fc01c3e97
longzhe <dragonlong1986@126.com>
2018-02-21 20:58:37 +0800

    polish

--- study-code/pyplot-code/pyplot_animation_1.py
+++ study-code/pyplot-code/pyplot_animation_1.py
@@ -10,7 +10,7 @@ from matplotlib.animation import FuncAnimation
 fig, ax = plt.subplots()
 
 x_data, y_data = [], []
-ln, = ax.plot([], [], 'r-', animated=False)
+line, = ax.plot([], [], 'r-')
 
 
 def init_line():
@@ -18,18 +18,17 @@ def init_line():
     ax.set_xlim(0, 2 * np.pi)
     # 同等于 fig.ylim
     ax.set_ylim(-1, 1)
-    return ln,
+    return line,
 
 
 def update(frame):
     x_data.append(frame)
     y_data.append(np.sin(frame))
-    ln.set_data(x_data, y_data)
-    return ln,
+    line.set_data(x_data, y_data)
+    return line,
 
 
 # blit决定是更新整张图的点(False)还是只更新变化的点(True)
 ani = FuncAnimation(fig, update, frames=np.linspace(0, 2 * np.pi, 128),
                     init_func=init_line, blit=True, interval=50)
 plt.show()
-
